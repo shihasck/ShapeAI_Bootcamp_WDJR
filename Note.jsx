8ed972fdaa5ee33130ef74cdafb0bbf16cb590b9
Shihas <shihascse@gmail.com>
2021-06-05 12:29:11 +0400

    source files

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>
+        Web Dev with Javascript and React.js - Free 7 days Online Bootcamp
+      </h1>
+      <p>
+        Here we bring our another free 7 days live BOOTCAMP with a hands-on
+        project at the end of Front-End Web Dev with Javascript and React.js-
+        The Foundation of Frontend Web Development 🖥
+      </p>
+    </div>
+  );
+}
+
+export default Note;
